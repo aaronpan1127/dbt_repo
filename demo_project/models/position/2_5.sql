63ac6ddf089fe909d3437d4eb4678bf4fae89ee0
Pan <aaron.pan@glencore.com.au>
2023-04-20 18:25:52 +1000

    dfad

---
+++ demo_project/models/position/2_5.sql
@@ -0,0 +1,29 @@
+
+/*
+    Welcome to your first dbt model!
+    Did you know that you can also configure models directly within SQL files?
+    This will override configurations stated in dbt_project.yml
+
+    Try changing "table" to "view" below
+*/
+
+{{ config(materialized='table') }}
+
+with final as (
+
+    select id
+    from {{ ref('1_5') }}
+    union all
+    select id
+    from {{ ref('1_6') }}    
+
+)
+
+select *
+from final
+
+/*
+    Uncomment the line below to remove records with null `id` values
+*/
+
+-- where id is not null
